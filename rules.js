cd98a65638c1699afc8f7a8ce2e5430010dbbfa2
gabrielj <gabrielj@udel.edu>
2016-01-13 19:58:02 +0000

    Moved <script> to bottom so js will load after DOM is ready. Edited comments.

--- rules.js
+++ rules.js
@@ -7,24 +7,14 @@ var temp = function(){
     // console.log("This is temporary");
     alert("Added to log: TEMP FUNC");
 };
-var temp0 = function(ev){
-    // console.log("This is temporary");
-    alert("Added to log: TEMP FUNC0");
-};
 
 var $body = document.querySelectorAll('body');
 
 var $ruleButton = document.getElementById('rule_check');
 $ruleButton.addEventListener('click', temp);
-// $ruleButton.addEventListener('click', temp0);
-// $ruleButton.addEventListener('click', function(ev){
-//                                 alert("TEMP FUNC");
-//                             });
-// document.getElementById('rule_check').addEventListener('click', temp);
-// document.getElementById('rule_check').addEventListener('click', function(ev){
-// alert();})
 
 // David - ^^^^^^^^^^^ for the event listener, we need to make it so that only once the element is made
     //does it try to add the event listener
         //window.onload?
-        //document DOM ready
+        //document DOM ready - onload?
+    // or move <script> to bottom of html document
