b381d89b1085d7ca0c0083262be74a7922238bfc
gabrielj <gabrielj@udel.edu>
2016-01-13 19:37:21 +0000

    Add event listener to button for testing. Moved <script src="rules.js"> to <head>

--- rules.js
+++ rules.js
@@ -1,3 +1,25 @@
-<script type="text/javascript">
+// <script type="text/javascript">
 
-</script>
+// </script>
+// ^^^^ David - Uneeded in js file
+
+var temp = function(){
+    // console.log("This is temporary");
+    alert("Added to log: TEMP FUNC");
+};
+var temp0 = function(ev){
+    // console.log("This is temporary");
+    alert("Added to log: TEMP FUNC0");
+};
+
+var $body = document.querySelectorAll('body');
+
+// document.getElementById('rule_check').addEventListener('click', function(ev){
+// alert();})
+var $ruleButton = document.getElementById('rule_check');
+$ruleButton.addEventListener('click', temp);
+// $ruleButton.addEventListener('click', temp0);
+// $ruleButton.addEventListener('click', function(ev){
+//                                 alert("TEMP FUNC");
+//                             });
+// document.getElementById('rule_check').addEventListener('click', temp);
